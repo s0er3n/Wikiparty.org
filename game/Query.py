aff3f8724277feca23e077a21407f9b52c7d959b
Sören Michaels <soeren.michaels@gmail.com>
2022-12-17 18:32:55 +0100

    caching queries

--- game/Query.py
+++ game/Query.py
@@ -1,9 +1,13 @@
-from game.Player import Player
-import requests
-from game.ConnectionManager import manager
-from game.Response import Response, Wiki
 import asyncio
 from threading import Thread
+from typing import Any
+
+import requests
+
+from game.ConnectionManager import manager
+from game.Player import Player
+from game.Response import Response, Wiki
+
 # from collections import defaultdict
 
 
@@ -11,15 +15,31 @@ class Query:
     # i will query one by one for MVP
     # next_query: dict[str, list[Player]] = defaultdict(list)
 
-    @staticmethod
-    def execute(move: str, recipient: Player):
-        # self.next_query[move].apend(recipient)
-        r = requests.get(
-            f"https://en.wikipedia.org/w/api.php?action=parse&page={move}&format=json")
-        data = r.json()
+    queries: dict[str, Any] = dict()
 
-        thread = Thread(target=asyncio.run, args=(manager.send_response(
-            Response(method="move", data=Wiki(
-                data=data["parse"]), recipients=[recipient])
-        ),))
+    @classmethod
+    def execute(cls, move: str, recipient: Player):
+        # self.next_query[move].apend(recipient)
+        if not cls.queries.get(move):
+            r = requests.get(
+                f"https://en.wikipedia.org/w/api.php?action=parse&page={move}&format=json"
+            )
+            data = r.json()
+
+            if len(cls.queries) > 500:
+                cls.queries.clear()
+            cls.queries[move] = data["parse"]
+
+        thread = Thread(
+            target=asyncio.run,
+            args=(
+                manager.send_response(
+                    Response(
+                        method="move",
+                        data=Wiki(data=cls.queries.get(move)),
+                        recipients=[recipient],
+                    )
+                ),
+            ),
+        )
         thread.start()
