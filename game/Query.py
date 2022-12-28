51bbe1a5fee9aff18455187af0ea8b6b7688ed35
tilman <tilman.grueger@web.de>
2022-12-28 11:37:00 +0100

    -unuesed lines

--- game/Query.py
+++ game/Query.py
@@ -13,33 +13,11 @@ from game.ConnectionManager import manager
 from game.Player import Player
 from game.Response import Response, Wiki
 
-# from collections import defaultdict
-
 
 def _select_and_reduce_links(all_links):
     for link in all_links:
         if link["href"].startswith("/wiki/") and not link["href"].startswith("/wiki/Help") and not link["href"].startswith("/wiki/File"):
             yield link["href"][6::]
-
-
-def _skip_if_redirect(data):
-    soup = BeautifulSoup(data["parse"]["text"]["*"], "html.parser")
-    length = len(soup.find_all("ul", {"class": "redirectText"}))
-    if length == 1:
-        href = soup.find("ul", {"class": "redirectText"}
-                         ).find("a", href=True)["href"]
-        redirect = href.split("/")[2]
-        logging.info(f"redirected to {redirect}")
-        r = requests.get(
-            f"https://en.wikipedia.org/w/api.php?action=parse&page={redirect}&format=json"
-        )
-        return r.json()
-    if length > 1:
-        logging.warning(
-            "found multiple redirects on redirect page, returning redirect page"
-        )
-    return data
-    logging.error("wtf happend")
 
 
 class Query:
@@ -49,7 +27,7 @@ class Query:
     queries: dict[str, Any] = dict()
 
     @classmethod
-    def _add_move_to_queries(cls, move: str):
+    def _query_and_add_to_queries(cls, move: str):
         logging.warning(f"add move to query {move}")
         resp_text = requests.get(
             f"https://en.wikipedia.org/wiki/{move}"
@@ -60,23 +38,12 @@ class Query:
         title = soup.find("h1").text
 
         article = soup.find("div", {"id": "mw-content-text"})
-        print(article)
+
         # data = _skip_if_redirect(data)
 
         all_links = soup.find_all("a", href=True)
 
-        # for link in all_links:
-        #     if link["href"].startswith("/wiki/") and not link["href"].startswith("/wiki/Help") and not link["href"].startswith("/wiki/File"):
-        #         yield link["href"][6::]
-
         short_links = list(_select_and_reduce_links(all_links))
-
-        # reduced_links = list(map(lambda a: a["href"], all_links))
-        #
-        # links = list(filter(lambda c: (c.startswith("/wiki/") and "/wiki/Help" not in c and "/wiki/File" not in c),
-        #                     reduced_links))
-        #
-        # short_links = list(map(lambda a: a[6::], links))
 
         if len(cls.queries) > 500:
             cls.queries.clear()
@@ -84,15 +51,13 @@ class Query:
                              "title": str(title),
                              "content_html": str(article),
                              "url_ending": move}
-        print(cls.queries[move]["title"])
 
     @classmethod
     def execute(cls, move: str, recipient: Player):
-        # self.next_query[move].apend(recipient)
         if not cls.queries.get(move):
             logging.warning(f"before try move {move}")
             try:
-                cls._add_move_to_queries(move)
+                cls._query_and_add_to_queries(move)
             except:
                 logging.error("could not query wikipedia")
                 return
@@ -115,5 +80,4 @@ class Query:
 
 def test_query():
     target = "berlin"
-    print(Query._add_move_to_queries(target))
-    assert Query._add_move_to_queries(target)
+    assert Query._query_and_add_to_queries(target)
