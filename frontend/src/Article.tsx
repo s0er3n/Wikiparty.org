4bf59aa787caf7f00a39d35896324df1dd7be978
Sören Michaels <soeren@Air-von-Soren.fritz.box>
2023-01-08 11:09:05 +0100

    ui improvement

--- frontend/src/Article.tsx
+++ frontend/src/Article.tsx
@@ -3,9 +3,7 @@ import { Component } from "solid-js";
 const Article: Component<{ title: string; points?: number }> = (props) => {
   return (
     <>
-      <b>
-        {props.title} <i>{props.points ?? ""}</i>
-      </b>
+      <b>{props.title}</b> {props.points ? "(" + props.points + ")" : ""}
     </>
   );
 };
