f6ae70170fcfbdc6dc1c8e79da217938f97cf127
Sören Michaels <soeren@Air-von-Soren.fritz.box>
2023-01-17 00:28:42 +0100

    redis

--- game/Query.py
+++ game/Query.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
+import redis
 from threading import Thread
 from typing import Iterator
-
+import json
 import requests
 from bs4 import BeautifulSoup
 
@@ -18,11 +19,10 @@ def _select_and_reduce_links(all_links) -> Iterator[str]:
             yield link["href"][6::]
 
 
-class Query:
-    # i will query one by one for MVP
-    # next_query: dict[str, list[Player]] = defaultdict(list)
+redis_client = redis.Redis(host="localhost", port=6379, db=0)
 
-    queries: dict[str, QueryResult] = dict()
+
+class Query:
 
     @classmethod
     def query_and_add_to_queries(cls, move: str) -> str | None:
@@ -35,7 +35,7 @@ class Query:
         if resp.history:
             move = resp.history[-1].url
 
-        soup = BeautifulSoup(resp_text, "html.parser")
+        soup = BeautifulSoup(resp_text, "lxml")
 
         if not (h1 := soup.find("h1")):
             logging.warning("no h1")
@@ -50,17 +50,15 @@ class Query:
 
         short_links = list(_select_and_reduce_links(all_links))
 
-        if len(cls.queries) > 500:
-            cls.queries.clear()
-        cls.queries[move] = {"links": short_links,
-                             "title": str(title),
-                             "content_html": str(article),
-                             "url_ending": move}
+        redis_client.set(move, json.dumps({"links": short_links,
+                                           "title": str(title),
+                                           "content_html": str(article),
+                                           "url_ending": move}), ex=60 * 60 * 24 * 14)
         return move
 
     @classmethod
     def execute(cls, move: str, recipient: Player) -> str | None:
-        if not cls.queries.get(move):
+        if not redis_client.get(move):
             logging.warning(f"before try move {move}")
             try:
                 move = cls.query_and_add_to_queries(move)
@@ -69,9 +67,10 @@ class Query:
                 logging.error("could not query wikipedia")
                 return
 
-        if not (query_result := cls.queries.get(move)):
+        if not (query_result := redis_client.get(move)):
             logging.warning("no query result")
             return None
+        query_result = json.loads(query_result)
         thread = Thread(
             target=asyncio.run,
             args=(
@@ -86,8 +85,17 @@ class Query:
         thread.start()
         return query_result["title"]
 
+    @classmethod
+    def is_link_allowed(cls, current_location, url_name) -> bool:
+        if not (redis_result := redis_client.get(current_location)):
+            # requerying current location in case it was not in redis
+            cls.query_and_add_to_queries(current_location)
+            redis_result = redis_client.get(current_location)
+
+        return url_name in json.loads(redis_result)["links"]
+
 
 def test_query() -> None:
     target = "berlin"
     Query.query_and_add_to_queries(target)
-    assert Query.queries.get(target) is not None
+    assert json.loads(redis_client.get(target)) is not None
--- game/RandomQuery.py
+++ game/RandomQuery.py
@@ -41,20 +41,21 @@ class Item(TypedDict):
 class RandomQuery:
     @staticmethod
     def execute(player: Player):
-        date = datetime.today().strftime('%Y/%m/%d')
-        random_number = random.randint(1, 1000)
-        r = requests.get(
-            f"http://wikirank-2022.di.unimi.it/Q/?filter%5Btext%5D=Harmonic+centrality&filter%5Bselected%5D=true&filter%5Bvalue%5D=harmonic&view=list&pageSize=10&pageIndex={random_number}&type=harmonic&score=false"
-        )
-        data: Response = r.json()
-        print(data)
-
-        if r.status_code == 200:
-            thread = Thread(
-                target=asyncio.run,
-                args=(manager.send_response(
-                    Random(data=[article["harmonic"].split(">")[1].split("<")[0] for article in data["items"]], _recipients=[player])),),
-            )
-            thread.start()
-        else:
-            raise Exception("Error while fetching data from wikipedia search")
+        pass
+        # date = datetime.today().strftime('%Y/%m/%d')
+        # random_number = random.randint(1, 1000)
+        # r = requests.get(
+        #     f"http://wikirank-2022.di.unimi.it/Q/?filter%5Btext%5D=Harmonic+centrality&filter%5Bselected%5D=true&filter%5Bvalue%5D=harmonic&view=list&pageSize=10&pageIndex={random_number}&type=harmonic&score=false"
+        # )
+        # data: Response = r.json()
+        # print(data)
+        #
+        # if r.status_code == 200:
+        #     thread = Thread(
+        #         target=asyncio.run,
+        #         args=(manager.send_response(
+        #             Random(data=[article["harmonic"].split(">")[1].split("<")[0] for article in data["items"]], _recipients=[player])),),
+        #     )
+        #     thread.start()
+        # else:
+        #     raise Exception("Error while fetching data from wikipedia search")
