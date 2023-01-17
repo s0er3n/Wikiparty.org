8c681d26edd39cb28e311d201c76cf140053e49e
Sören Michaels <soeren@Air-von-Soren.fritz.box>
2023-01-17 20:00:55 +0100

    redis from url

--- game/db.py
+++ game/db.py
@@ -1,3 +1,10 @@
 import redis
 
-client = redis.Redis(host="localhost", port=6379, db=0)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.environ["redis_url"]
+
+client = redis.from_url(url)
