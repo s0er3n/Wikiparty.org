5f70aa9bf1044dd12d959aa5f8f99e0b338fd93b
Sören Michaels <soeren@Air-von-Soren.fritz.box>
2023-01-22 17:34:16 +0100

    icons

--- frontend/src/lobby/Header.tsx
+++ frontend/src/lobby/Header.tsx
@@ -43,7 +43,7 @@ const Header: Component<{
                 };
                 sendMessage(leaveMsg);
               }}
-              class="hidden sm:block btn btn-ghost normal-case text-xl"
+              class="hidden text-center sm:block btn btn-ghost normal-case text-xl"
             >
               WikiParty.org (alpha)
             </a>
@@ -72,7 +72,21 @@ const Header: Component<{
                 }}
                 class="btn"
               >
-                copy
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-clipboard"
+                >
+                  <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+                  <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+                </svg>
               </button>
             </Show>
           </div>
--- frontend/src/lobby/NavigationButtons.tsx
+++ frontend/src/lobby/NavigationButtons.tsx
@@ -9,27 +9,50 @@ export const BackButton: Component = () => {
   return (
     <button
       onclick={async () => {
-        sendMessage(back_page_msg)
+        sendMessage(back_page_msg);
       }}
       class="btn"
     >
-
-      Back
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-chevron-left"
+      >
+        <polyline points="15 18 9 12 15 6"></polyline>
+      </svg>
     </button>
-  )
-}
+  );
+};
 
 export const ForwardButton: Component = () => {
   return (
     <button
       onclick={async () => {
-        sendMessage(forward_page_msg)
+        sendMessage(forward_page_msg);
       }}
       class="btn"
     >
-
-      Forward
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-chevron-right"
+      >
+        <polyline points="9 18 15 12 9 6"></polyline>
+      </svg>
     </button>
-  )
-}
-
+  );
+};
