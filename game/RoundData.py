4e12f1ce240d8242a721c3227d821bfa1b6b67c0
tilman <tilman.grueger@web.de>
2023-02-24 12:08:48 +0100

    more stuff

---
+++ game/RoundData.py
@@ -0,0 +1,91 @@
+from dataclasses import dataclass, field
+from collections import defaultdict
+
+from game.Article import Article
+from game.Player.Player import Player
+from game.Player.PlayerData import Node, sorted_moves_list
+
+
+@dataclass
+class Moves:
+    start: Node
+    current: Node
+
+    def get_moves(self) -> list[Article]:
+        return [node.article for node in sorted_moves_list(self.start, [])]
+
+    def go_back(self) -> None:
+        if self.current.parent is not None:
+            self.current = self.current.parent
+
+    def go_forward(self) -> None:
+        if self.current.children:
+            self.current = self.current.children[-1]
+
+
+def create_defaultdict():
+    return defaultdict(int)
+
+
+@dataclass
+class RoundData:
+    """Data for a single round of the game."""
+
+    points: dict[Player, int] = field(default_factory=create_defaultdict)
+
+    moves: dict[Player, Moves] = field(default_factory=dict)
+
+    articles_to_find: set[Article] = field(default_factory=set)
+
+    found_articles: set[Article] = field(default_factory=set)
+
+    start_article: Article = Article("", "")
+
+    end_time: int = 0
+
+    def get_articles_to_find_pretty_name(self) -> set[str]:
+        return set(article.pretty_name for article in self.articles_to_find)
+
+    def get_articles_to_find_description(self) -> dict[str, str]:
+        return {article.pretty_name:  article.description for article in self.articles_to_find}
+
+    def get_found_articles_pretty_name(self) -> set[str]:
+        return set(article.pretty_name for article in self.found_articles)
+
+    def add_move(self, player: Player, article: Article):
+        """Add a move to the round data."""
+        if player not in self.moves:
+            start_node = Node(self.start_article, parent=None)
+            new_node = start_node.add_child(article)
+            self.moves[player] = Moves(start_node, new_node)
+        else:
+            self.moves[player].current = self.moves[player].current.add_child(
+                article)
+
+    def get_current_points(self, player: Player) -> int:
+        """Get the points of a player."""
+        if player not in self.points:
+            return 0
+        return self.points[player]
+
+    def get_moves(self, player: Player) -> list[Article]:
+        """Get the moves of a player."""
+        if player not in self.moves:
+            return []
+        return self.moves[player].get_moves()
+
+    def go_back(self, player: Player) -> None:
+        """Go back one move for a player."""
+        if player in self.moves:
+            self.moves[player].go_back()
+
+    def go_forward(self, player: Player) -> None:
+        """Go forward one move for a player."""
+        if player in self.moves:
+            self.moves[player].go_forward()
+
+    def get_current_article(self, player: Player) -> Article:
+        """Get the current article of a player."""
+        if player in self.moves:
+            return self.moves[player].current.article
+        return self.start_article
--- game/SearchGame.py
+++ game/SearchGame.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-from collections import defaultdict
 from threading import Thread
 from time import sleep
 import requests
@@ -11,123 +10,14 @@ from game.ConnectionManager import manager
 from game.Game import Game
 from game.GameState import State
 from game.Player.Player import Player, PlayerCopy
-from game.Player.PlayerData import PlayerData, PlayerRights,  PlayerDataNoNode, Node, sorted_moves_list
+from game.Player.PlayerData import PlayerData, PlayerRights,  PlayerDataNoNode
 from game.Query.Query import Query
 from game.Response import Error, LobbyUpdate, Response, SyncMove
 from game.Player.PlayersHandler import PlayersHandler
-from dataclasses import dataclass, field
+from game.RoundData import RoundData
+from game.PointsCounter import PointsCounter
 
 logging.getLogger().setLevel(logging.INFO)
-
-
-@dataclass
-class Moves:
-    start: Node
-    current: Node
-
-    def get_moves(self) -> list[Article]:
-        return [node.article for node in sorted_moves_list(self.start, [])]
-
-    def go_back(self) -> None:
-        if self.current.parent is not None:
-            self.current = self.current.parent
-
-    def go_forward(self) -> None:
-        if self.current.children:
-            self.current = self.current.children[-1]
-
-
-def create_defaultdict():
-    return defaultdict(int)
-
-
-@dataclass
-class RoundData:
-    """Data for a single round of the game."""
-
-    points: dict[Player, int] = field(default_factory=create_defaultdict)
-
-    moves: dict[Player, Moves] = field(default_factory=dict)
-
-    articles_to_find: set[Article] = field(default_factory=set)
-
-    found_articles: set[Article] = field(default_factory=set)
-
-    start_article: Article = Article("", "")
-
-    end_time: int = 0
-
-    def get_articles_to_find_pretty_name(self) -> set[str]:
-        return set(article.pretty_name for article in self.articles_to_find)
-
-    def get_articles_to_find_description(self) -> dict[str, str]:
-        return {article.pretty_name:  article.description for article in self.articles_to_find}
-
-    def get_found_articles_pretty_name(self) -> set[str]:
-        return set(article.pretty_name for article in self.found_articles)
-
-    def add_move(self, player: Player, article: Article):
-        """Add a move to the round data."""
-        if player not in self.moves:
-            start_node = Node(self.start_article, parent=None)
-            new_node = start_node.add_child(article)
-            self.moves[player] = Moves(start_node, new_node)
-        else:
-            self.moves[player].current = self.moves[player].current.add_child(
-                article)
-
-    def get_current_points(self, player: Player) -> int:
-        """Get the points of a player."""
-        if player not in self.points:
-            return 0
-        return self.points[player]
-
-    def get_moves(self, player: Player) -> list[Article]:
-        """Get the moves of a player."""
-        if player not in self.moves:
-            return []
-        return self.moves[player].get_moves()
-
-    def go_back(self, player: Player) -> None:
-        """Go back one move for a player."""
-        if player in self.moves:
-            self.moves[player].go_back()
-
-    def go_forward(self, player: Player) -> None:
-        """Go forward one move for a player."""
-        if player in self.moves:
-            self.moves[player].go_forward()
-
-    def get_current_article(self, player: Player) -> Article:
-        """Get the current article of a player."""
-        if player in self.moves:
-            return self.moves[player].current.article
-        return self.start_article
-
-
-class PointsCounter:
-    """Counts points for a single round."""
-
-    def __init__(self, round_data: RoundData) -> None:
-        self.round_data = round_data
-
-    def set_new_round(self, round_data: RoundData) -> None:
-        self.round_data = round_data
-
-    def check_for_points_on_move(self, player: Player, article: Article) -> None:
-        player_moves = [
-            article.pretty_name for article in self.round_data.get_moves(player)]
-        if article.pretty_name in self.round_data.get_articles_to_find_pretty_name() and article.pretty_name not in player_moves:
-            if not self.round_data.points.get(player):
-                self.round_data.points[player] = 0
-            self.round_data.points[player] += 10
-            logging.info(
-                f"Player {player.name} found article {article.pretty_name} + 10 points")
-            if article.pretty_name not in self.round_data.get_found_articles_pretty_name():
-                self.round_data.points[player] += 5
-                self.round_data.found_articles.add(article)
-                logging.info(
-                    f"Player {player.name} found article {article.pretty_name} + 5 points")
 
 
 class RoundEndChecker:
