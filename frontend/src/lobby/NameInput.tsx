6c75e1d0799c17b46f0476668a66f426a0f43fde
tilman <tilman.grueger@web.de>
2023-03-01 13:44:28 +0100

    only click on own name & fixes

--- frontend/src/lobby/NameInput.tsx
+++ frontend/src/lobby/NameInput.tsx
@@ -1,36 +1,33 @@
 import { Component, createSignal } from "solid-js";
 
-import { setContainer, setChangeName, userName, setUserName } from "./PlayerList"
+import { userName, setUserName } from "./PlayerList"
 import { setUserNameMsg } from "../SetUserName";
 import { sendMessage } from "../App";
 
-const [tempUsername, setTempUsername] = createSignal<string>()
 
 const NameInput: Component = () => {
-  setTempUsername(userName())
+  const [tempUsername, setTempUsername] = createSignal<string>(userName())
 
   return (
     <>
-      < input type="checkbox" id="my-modal-6" class="modal-toggle" />
+      <input type="checkbox" id="my-modal-6" class="modal-toggle" />
       <div class="modal modal-bottom sm:modal-middle">
         <div class="modal-box">
-          <h3 class="font-bold text-lg">Change user name</h3>
+          <h3 class="font-bold text-lg">Change username</h3>
           <form>
             <input
-              ref={setContainer}
               value={tempUsername()}
               minlength="2"
               maxlength="12"
               onchange={(e) => {
                 setTempUsername(e.target.value)
               }}
-              onblur={() => setChangeName(false)}
             >
             </input>
             <div class="modal-action">
               <label onclick={() => {
                 setTempUsername(userName())
-              }} for="my-modal-6" class="btn">cancle</label>
+              }} for="my-modal-6" class="btn">Cancle</label>
               <label onclick={() => {
                 let msg = setUserNameMsg;
                 setUserName(tempUsername())
@@ -38,13 +35,13 @@ const NameInput: Component = () => {
                 sendMessage(msg);
                 localStorage.setItem("username", tempUsername());
               }} for="my-modal-6" class="btn">
-                Yay!
+                Submit
               </label>
 
             </div>
           </form>
         </div>
-      </div >
+      </div>
     </>
   )
 }
--- frontend/src/lobby/PlayerList.tsx
+++ frontend/src/lobby/PlayerList.tsx
@@ -1,7 +1,7 @@
 import { Accessor, Component, For, Show, createSignal } from "solid-js";
 import { TPlayer } from "../types";
 
-export const [changeName, setChangeName] = createSignal<boolean>(false);
+// export const [changeName, setChangeName] = createSignal<boolean>(false);
 export const [container, setContainer] = createSignal<HTMLDivElement>();
 export const [userName, setUserName] = createSignal<string>("");
 
@@ -23,11 +23,9 @@ const PlayerList: Component<{
       {(player: TPlayer) => (
         <>
           <div class="w-fit">
-            <label for="my-modal-6" >
+            <Show when={player[0].id == props.id} fallback={
               <span class="font-light" onclick={() => {
                 if (player[0].id == props.id) {
-                  setChangeName(true)
-                  container()?.focus()
                 }
               }
               }>
@@ -35,7 +33,19 @@ const PlayerList: Component<{
                   ? player[0].name.substring(0, 12)
                   : player[0].name}
               </span>
-            </label >
+            }>
+              <label for="my-modal-6" >
+                <span class="font-light" onclick={() => {
+                  if (player[0].id == props.id) {
+                  }
+                }
+                }>
+                  {player[0].name.length > 12
+                    ? player[0].name.substring(0, 12)
+                    : player[0].name}
+                </span>
+              </label >
+            </Show>
 
 
             :<span class="font-bold"> {player[0][props.pointsKey] ?? 0} </span>
